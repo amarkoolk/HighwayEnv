4ee652e10b64b512bbade33fef419b420f793f88
Amar Kulkarni <ark8su@virginia.edu>
2023-04-17 23:06:14 -0400

    crash env

--- highway_env/__init__.py
+++ highway_env/__init__.py
@@ -26,6 +26,12 @@ def register_highway_envs():
         entry_point='highway_env.envs:HighwayEnvFast',
     )
 
+    # crash_env.py
+    register(
+        id='crash-v0',
+        entry_point='highway_env.envs:CrashEnv',
+    )
+
     # intersection_env.py
     register(
         id='intersection-v0',
